d2693fab891f9f49fdcf88b54ccc378b93c38656
masterofdon <aerdemekin@gmail.com>
2017-12-15 00:00:52 +0300

    Inital add cont
    Change root file structure
    Add Spring Project
    Change configs

---
+++ src/test/java/com/outz/app/OutzTsaAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.outz.app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OutzTsaAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
